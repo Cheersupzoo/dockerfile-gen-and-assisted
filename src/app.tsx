f706b7c9cc5739194a350158f402f2c204e3992b
Suppachai Thanrukprasert <54928306+Cheersupzoo@users.noreply.github.com>
2022-10-23 16:36:36 +0700

    setup react properly

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+// import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Index } from "./client";
 
 function render() {
-  ReactDOM.render(<Index />, document.body);
+  const root = createRoot(document.getElementById("app"));
+  root.render(<Index />);
 }
 
 render();
